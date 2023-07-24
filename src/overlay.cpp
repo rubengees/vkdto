96d2b501f492f5158fb8f1f7abd4e37b4358ff37
Ruben Gees <rubengees7@gmail.com>
2023-07-24 19:55:25 +0200

    Fix compilation error on newer vulkan versions

--- src/overlay.cpp
+++ src/overlay.cpp
@@ -21,6 +21,14 @@
  * IN THE SOFTWARE.
  */
 
+#if defined(__GNUC__) && __GNUC__ >= 4
+#define VK_LAYER_EXPORT __attribute__((visibility("default")))
+#elif defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590)
+#define VK_LAYER_EXPORT __attribute__((visibility("default")))
+#else
+#define VK_LAYER_EXPORT
+#endif
+
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
